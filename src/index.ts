c7429ce1665183281aef0df5bed485133bf99668
bsiafas10 <brunosiafas@gmail.com>
2023-12-22 16:27:00 -0300

    Inicializar proyecto

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
+// src/index.ts
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
