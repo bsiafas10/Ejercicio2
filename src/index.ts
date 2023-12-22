e6475c1335f2b1f9132f54916837cba0bb2d242b
bsiafas10 <brunosiafas@gmail.com>
2023-12-22 16:37:59 -0300

    Inicializar proyecto

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,51 @@
 // src/index.ts
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+app.use(express.json());
+
+app.post('/calcular-tasa', (req: Request, res: Response) => {
+  try {
+    const { marca, monto } = req.body;
+
+    if (!marca || !monto) {
+      throw new Error('La solicitud debe incluir la marca de la tarjeta y el monto de la operación.');
+    }
+
+    const tasa = calcularTasa(marca, parseFloat(monto));
+
+    res.json({ tasa });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: 'Error desconocido.' });
+    }
+  }
 });
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
+
+function calcularTasa(marca: string, monto: number): number {
+  let tasa = 0;
+
+  switch (marca.toUpperCase()) {
+    case 'VISA':
+      tasa = monto * 0.02;
+      break;
+    case 'NARA':
+      tasa = monto * 0.015;
+      break;
+    case 'AMEX':
+      tasa = monto * 0.03;
+      break;
+    default:
+      throw new Error('Marca de tarjeta no válida. Las opciones son: VISA, NARA, AMEX.');
+  }
+
+  return tasa;
+}
